00b6771e32faf44c1d3bfcbde2c6705c3ff84fad
Slsv1 <slava.svinota@gmail.com>
2025-06-06 19:41:44 +0200

    work on todo app and small rework in AppData class

--- component.py
+++ component.py
@@ -24,11 +24,13 @@ class DataID:
     data: tuple[tuple[Direction, int], ...]
     def child(self, id: int, direction: None | Direction = None) -> Self:
         if len(self.data):
-            return self.__class__((*self.data, (self.data[-1][0], id)))
-        return self.__class__((*self.data, (direction or Direction.VERTICAL, id)))
+            return self.__class__((*self.data, (self.data[-1][0] if direction is None else direction, id)))
+        return self.__class__((*self.data, (direction if direction is not None else Direction.VERTICAL, id)))
     @property
     def direction(self):
         return self.data[-1][0]
+    def __bool__(self):
+        return bool(len(self.data))
 
 def root_vertical():
     return DataID(((Direction.VERTICAL, 0),))
@@ -37,76 +39,94 @@ def try_find_nearest(nav_data: list[DataID], current_index: int, direction: Dire
     next_index = current_index
     advance = lambda: next_index + (-1 if backwards else 1)
     next_index = advance()
-    print("supposed next", next_index)
 
     original_depth = len(nav_data[current_index].data)
-    nested = False
+    changed_directions = False
 
     while True:
+        # if next index is out of bounds
         if next_index >= len(nav_data) or next_index < 0:
-            print("out of bounds", next_index)
             return None
-        if nav_data[next_index].direction != direction:
-            nested = True 
-            print("wheeeat!")
+        
+        # if next index parent is a different direction then inputed,
+        # in this case just keep advancing index untill either end of nav_data or direction matches and nav_depth is same or less than original
+        if nav_data[next_index].data[-2][0] != direction:
+            changed_directions = True 
             next_index = advance()
+            print("direction is different")
             continue
-        if nested and len(nav_data[next_index].data) > original_depth:
-            print("nested!!!")
+        if changed_directions and (len(nav_data[next_index].data) > original_depth):
             next_index = advance()
+            print("depth is different")
             continue
         return next_index
+
+def visualise_nav_data(d: list[DataID], selected: DataID = DataID(())):
+    return "\n".join([
+        (
+            (">" if i == selected else "")
+            + ".".join(f'{"V" if j[0] == Direction.VERTICAL else "H"}{j[1]}' for j in i.data)
+        ) for i in d
+    ])
 
 @dataclass
 class AppState:
     mouse_position: Coordinate = Coordinate(-1, -1)
     _next_data_id: DataID = DataID(())
+    _data_id_to_index: dict[DataID, int] = field(default_factory=dict)
     _nav_data: list[DataID] = field(default_factory=list)
-    _current_selected_index: int = -1
     _current_selected_dataid: DataID = DataID(())
 
     #
     # state management
     #
     def is_selected(self, key: DataID) -> bool:
-        return key == self._current_selected_dataid
+        return key.data == self._current_selected_dataid.data[: len(key.data)]
+
     def queue_to_become_selected(self, key: DataID):
-        print("queued", key)
         self._next_data_id = key
 
     def step(self, mouse_position: Coordinate=Coordinate(-1, -1), nav=Coordinate(0, 0)):
         self.mouse_position = mouse_position
-        self._current_selected_dataid = self._next_data_id
-        print(self._current_selected_dataid)
+
+        # current selected dataid can be either
+        #   same as in previous cycle
+        #   set to value in self._next_data_id (this happens when mouse_positoin overlaps with a read_box node)
+        #   incremented if nav is not Coordinte(0, 0)
+        self._current_selected_dataid = self._next_data_id if self._next_data_id else self._current_selected_dataid
         self._next_data_id = DataID(())
-        print(self._current_selected_dataid)
-    
+        print("prenavigation:")
+        print(visualise_nav_data(self._nav_data, self._current_selected_dataid))
 
         if (nav.x != 0 or nav.y != 0) and len(self._nav_data):
-            print("selected index before", self._current_selected_index)
-            if self._current_selected_index == -1:
-                self._current_selected_index = 0
+            if self._current_selected_dataid == DataID(()): # if no previous selected dataid
                 self._current_selected_dataid = self._nav_data[0]
-            direction = Direction.HORIZONTAL if nav.x else Direction.VERTICAL
-            if direction == Direction.HORIZONTAL:
-                backwards = True if nav.x < 0 else False
-            elif direction == Direction.VERTICAL:
-                backwards = True if nav.y < 0 else False
-            print(direction, backwards)
-            next_index = try_find_nearest(self._nav_data, self._current_selected_index, direction, backwards)
-            if next_index is not None:
-                print("found", next_index)
-                self._current_selected_index = next_index
-                self._current_selected_dataid = self._nav_data[next_index]
-                
-        print("selected index after", self._current_selected_index)
-        print("selected data after", self._current_selected_dataid)
+            else:
+
+                direction = Direction.HORIZONTAL if nav.x else Direction.VERTICAL
+                if direction == Direction.HORIZONTAL:
+                    backwards = True if nav.x < 0 else False
+                elif direction == Direction.VERTICAL:
+                    backwards = True if nav.y < 0 else False
+
+                next_index = try_find_nearest(self._nav_data, self._data_id_to_index[self._current_selected_dataid], direction, backwards)
+                if next_index is not None:
+                    print("next is:")
+                    print(visualise_nav_data([self._nav_data[next_index]]))
+
+                # if found next index
+                if next_index is not None:
+                    self._current_selected_dataid = self._nav_data[next_index]
+
         self._nav_data.clear()
+        self._data_id_to_index.clear()
         
     #
     # nodes
     #
     def interaction(self, key: DataID):
+        # important to add entry to dictionary before appending to nav data, so that key is same as index
+        self._data_id_to_index[key] = len(self._nav_data)
         self._nav_data.append(key)
         @applicable
         def _out(child: Node):
