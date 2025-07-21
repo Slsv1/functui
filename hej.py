a6dc95dc6c34458f70df05904319e7b365c50409
Slsv15 <133392473+Slsv1@users.noreply.github.com>
2025-07-21 23:10:54 +0200

    using functions instead of strings for names in nodes

--- hej.py
+++ hej.py
@@ -7,13 +7,23 @@ from typing import Callable
 
 # print(render(20, 20, border ** border ** adaptive_text(LOREM)))
 # print(render(20, 20, border ** border ** adaptive_text(LOREM)))
-print(render(100, 15,\
+# print(render(100, 20,\
+#     border ** vbox_flex([
+#         flex ** border ** adaptive_text(LOREM),
+#         no_flex ** hbar(),
+#         flex ** text("hej")
+#     ])
+# ))
+print(render(100, 20,\
     border ** vbox_flex([
-        flex ** border ** adaptive_text(LOREM),
+        flex ** fg(Color.RED) ** border ** adaptive_text(LOREM),
         no_flex ** hbar(),
-        flex ** text("hej")
+        flex ** border ** hbox([
+             text("hejsan"),
+             vbar(),
+             text("hejs\nhej"),
+        ])
     ])
 ))
 print(_border_render.cache_info())
-
 
