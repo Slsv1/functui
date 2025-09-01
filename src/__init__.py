bb4ac71b1c3fc5b339c7cd4e1b53de33d5e4ba02
Slsv15 <133392473+Slsv1@users.noreply.github.com>
2025-09-01 22:41:17 +0200

    removed hash parameter from Node class in favour of using partial args

--- src/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import default_elements
-from . import nav_elements
-from . import classes
-from .classes import applicable, Rect, Box, Coordinate
-from .nav import NavState, NavAction, InteractibleID, EMPTY_INTERACTIBLE, ROOT_HORIZONTAL, ROOT_VERTICAL, Direction
-from .textfield import TextAction, TextActionChar, TextInput, start_text_input
-from .ansirender import layout_to_str, layout_to_result, result_to_str
