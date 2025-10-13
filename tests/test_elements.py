4039597ad6c4251f70a3d3d2d0cbb837699fc3ec
Viacheslav Andrushenko <133392473+Slsv1@users.noreply.github.com>
2025-10-13 22:56:54 +0200

    split by segments

--- tests/test_elements.py
+++ tests/test_elements.py
@@ -1,58 +1,74 @@
 import pytest
 # from functui.ansirender import layout_to_str
-from functui import Rect, layout_to_str
-from functui.default_elements import adaptive_text, _wrap_segments
+from functui import Rect, layout_to_str, Style
+# from functui.default_elements import adaptive_text, _wrap_segments
+from functui.classes import Color
+from functui.text_wrapping_elements import _span_to_segments, _Segment, _Span
 from wcwidth import wcswidth
 
 measure_text = lambda s: wcswidth(s)
 
-# def test_split_by_lines_respect_newlines():
-#     assert _wrap_segments(measure_text, 7, "foo\n\nhej bars") == [
-#         "foo",
-#         "",
-#         "hej ",
-#         "bars"
-#     ]
-#
-# def test_split_by_lines():
-#     assert _wrap_segments(measure_text, 6, "12345 12 456 123") == [
+def test_span_to_segments():
+    s1 = Style()
+    s2 = Style(fg=Color.BLUE)
+    assert _span_to_segments(
+        _Span(
+            (
+                "hej ",
+                _Span(
+                    ("blue",),
+                    s2
+                )
+            ),
+            s1
+        )
+    ) == [[_Segment("hej", s1), _Segment(" ", s1), _Segment("blue", s2)]]
+
+def test_span_to_segments_with_newline():
+    s1 = Style()
+    s2 = Style(fg=Color.BLUE)
+    assert _span_to_segments(
+        _Span(
+            (
+                "hej \nhej2 ",
+                _Span(
+                    ("blue",),
+                    s2
+                )
+            ),
+            s1
+        )
+    ) == [
+        [_Segment("hej", s1), _Segment(" ", s1)],
+        [_Segment("hej2", s1), _Segment(" ", s1), _Segment("blue", s2)]]
+
+
+# def test_adaptive_text_wrapping_remove_white_space():
+#     layout = adaptive_text("12345   12 456 123")
+#     assert layout_to_str(layout, Rect(6, 3)) == "\n".join([
 #         "12345 ",
 #         "12 456",
-#         "123"
-#     ]
-# def test_split_by_lines_too_long_word():
-#     assert _wrap_segmentds(measure_text, 4, "12345 12") == [
-#         "12345",
-#         "12"
-#     ]
-
-
-def test_adaptive_text_wrapping_remove_white_space():
-    layout = adaptive_text("12345   12 456 123")
-    assert layout_to_str(layout, Rect(6, 3)) == "\n".join([
-        "12345 ",
-        "12 456",
-        "123   ",
-    ])
-
-def test_adaptive_text_wrapping_word():
-    layout = adaptive_text("1234567")
-    assert layout_to_str(layout, Rect(6, 2)) == "\n".join([
-        "12345-",
-        "67    ",
-    ])
-
-def test_adaptive_text_wrapping_word_multiple():
-    layout = adaptive_text("1234567")
-    assert layout_to_str(layout, Rect(3, 2)) == "\n".join([
-        "12-",
-        "34-",
-        "567",
-    ])
-def test_adaptive_text_terminator():
-    layout = adaptive_text("1234 12345 123")
-    assert layout_to_str(layout, Rect(4, 2)) == "\n".join([
-        "1234",
-        "1...",
-    ])
-
+#         "123   ",
+#     ])
+#
+# def test_adaptive_text_wrapping_word():
+#     layout = adaptive_text("1234567")
+#     assert layout_to_str(layout, Rect(6, 2)) == "\n".join([
+#         "12345-",
+#         "67    ",
+#     ])
+#
+# def test_adaptive_text_wrapping_word_multiple():
+#     layout = adaptive_text("1234567")
+#     assert layout_to_str(layout, Rect(3, 2)) == "\n".join([
+#         "12-",
+#         "34-",
+#         "567",
+#     ])
+# def test_adaptive_text_terminator():
+#     layout = adaptive_text("1234 12345 123")
+#     assert layout_to_str(layout, Rect(4, 2)) == "\n".join([
+#         "1234",
+#         "1...",
+#     ])
+#
