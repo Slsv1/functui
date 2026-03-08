2ddc29be2015743fe83caef70f778f0db7209d69
Viacheslav Andrushenko <133392473+Slsv1@users.noreply.github.com>
2026-03-08 22:14:28 +0100

    vflex box wrap now uses new algorithm, and documentation example fixes

--- functui/flex.py
+++ functui/flex.py
@@ -1,7 +1,6 @@
 from dataclasses import dataclass
 from functools import lru_cache, partial
-from math import floor
-from typing import runtime_checkable, Callable, Iterable
+from typing import Callable, Iterable
 from .classes import *
 
 __all__ = [
@@ -28,15 +27,19 @@ def flex_custom(grow=1, shrink=True, basis=False) -> Callable[[Layout], Flex]:
     """Wrap child layout in a :obj:`Flex` class to mark it as flexible and adjust attributes.
 
     Note:
-        This node is only userful with flex containers (:obj:`vbox_flex` and :obj:`hbox_flex`).
+        This node is only useful with flex containers (:obj:`vbox_flex` and :obj:`hbox_flex`).
 
     Args:
         grow: 
-            How much unused space will be given to this layout relative to other layouts.
+            If there is any leftover space, this value is used to determine how
+            much of this space does this child get relative to other children.
         shrink:
-            This property has an effect only when ``basis`` is set to ``True``.
+            Should this child shrink if container does not fit into the
+            available space? This property has an effect only when ``basis`` is
+            set to ``True``.
         basis:
-            If parent container should reserve space for this layout's minum size.
+            If true, container will take this children's minimum size into account.
+            If false, container will assume minimum size of 0.
     """
 
     def out(node: Layout):
@@ -46,8 +49,10 @@ def flex_custom(grow=1, shrink=True, basis=False) -> Callable[[Layout], Flex]:
 def flex(node: Layout) -> Flex:
     """Wrap layout in a :obj:`Flex` class to mark it as flexible.
 
+    Equivalent to :obj:`flex_custom` ``(grow=1 shrink=True, basis=False)``.
+
     Note:
-        This node is only usefull with flex containers (:obj:`vbox_flex` and :obj:`hbox_flex`).
+        This node is only useful with flex containers (:obj:`vbox_flex` and :obj:`hbox_flex`).
     """
     return flex_custom(1, True, False)(node)
 
@@ -125,10 +130,10 @@ def _calculate_flex_children_sizes(
 def vbox_flex(children: Iterable[Flex | Layout]) -> Layout:
     """A container node that allows children to expand on the y axis.
 
-    Modeled of the CSS flexbox layout model. By default acts as a regular :obj:`~functui.common.vbox`.
-    If a child with the :obj:`flex` node as a parent is added, its height will be equal to the unused space on the y axis.
-    If multiple childred with the :obj:`flex` node are added, the remaing space will be devided equaly among them.
-    To destribute remaing space unevanly, childrens ``grow`` attribute can be changed by using :obj:`flex_custom`.
+    Modeled of the CSS flexbox layout model. By default acts as a regular
+    :obj:`~functui.common.vbox`. Wrap children in :func:`flex` or
+    :func:`flex_custom` wrapper nodes to allow them to use leftover space if
+    there is any.
 
     """
     children = tuple(child if isinstance(child, Flex) else flex_custom(0, False, True)(child) for child in children)
@@ -174,10 +179,10 @@ def _vbox_flex_render(children: tuple[Flex, ...], frame: Frame, box: Box):
 def hbox_flex(children: Iterable[Flex | Layout], /):
     """A container node that allows children to expand on the x axis.
 
-    Modeled of the CSS flexbox layout model. By default acts as a regular :obj:`~functui.common.xbox`.
-    If a child with the :obj:`flex` node as a parent is added, its width will be equal to the unused space on the x axis.
-    If multiple childred with the :obj:`flex` node are added, the remaing space will be devided equaly among them.
-    To destribute remaing space unevanly, childrens ``grow`` attribute can be changed by using :obj:`flex_custom`.
+    Modeled of the CSS flexbox layout model. By default acts as a regular
+    :obj:`~functui.common.hbox`. Wrap children in :func:`flex` or
+    :func:`flex_custom` wrapper nodes to allow them to use leftover space if
+    there is any.
 
     Examples:
         Usage with flex:
@@ -260,12 +265,13 @@ def _hbox_flex_render(children: Iterable[Flex], frame: Frame, box: Box):
 
     return res
 
+
 @dataclass
 class _FlexData:
     bounding_rect: Rect
     flex_children: list[Flex]
-
 def _split_flex_by_lines_h(available_space: int, children: Iterable[Flex], measure_text: MeasureTextFunc):
+
     flex_by_lines = [_FlexData(Rect(0, 0), [])]
 
 
@@ -276,7 +282,8 @@ def _split_flex_by_lines_h(available_space: int, children: Iterable[Flex], measu
             current_flex_data.flex_children.append(flex)
             continue
 
-        child_min_width, child_min_height = flex.node.min_size(measure_text, Rect(available_space, 9999))
+        child_min_width, child_min_height = flex.node.min_size(
+            measure_text, Rect(available_space, 999999))
 
         if current_flex_data.bounding_rect.width + child_min_width > available_space:
             flex_by_lines.append(_FlexData(
@@ -292,12 +299,12 @@ def _split_flex_by_lines_h(available_space: int, children: Iterable[Flex], measu
     return flex_by_lines
 
 def hbox_flex_wrap(children: Iterable[Flex | Layout]) -> Layout:
-    """A container node that allows children to expand on the y axis.
+    """A container node that allows children to wrap vertically.
 
-    Modeled of the CSS flexbox layout model. By default acts as a regular :obj:`~functui.common.vbox`.
-    If a child with the :obj:`flex` node as a parent is added, its height will be equal to the unused space on the y axis.
-    If multiple childred with the :obj:`flex` node are added, the remaing space will be devided equaly among them.
-    To destribute remaing space unevanly, childrens ``grow`` attribute can be changed by using :obj:`flex_custom`.
+    Modeled of the CSS flexbox layout model. If all children can't fit into the
+    available horizontal space, wrap them. Wrap children in :func:`flex` or
+    :func:`flex_custom` wrapper nodes to allow them to use leftover space if
+    there is any.
 
     """
     children = tuple(child if isinstance(child, Flex) else flex_custom(0, False, True)(child) for child in children)
@@ -314,6 +321,7 @@ def hbox_flex_wrap(children: Iterable[Flex | Layout]) -> Layout:
         render=partial(_hbox_flex_wrap_render, children)
     )
 
+
 @lru_cache(LRU_MAX_SIZE)
 def _hbox_flex_wrap_render(children: Iterable[Flex], frame: Frame, box: Box):
     #
@@ -323,25 +331,18 @@ def _hbox_flex_wrap_render(children: Iterable[Flex], frame: Frame, box: Box):
 
     at_y = 0
     res = Result()
+    results = []
 
     for data in children_by_lines:
-        children = data.flex_children
-        available_width = box.width - data.bounding_rect.width
-        row_height = data.bounding_rect.height
-
-        total_grow = sum(i.grow for i in children)
-        total_shrink = sum(i.shrink for i in children)
-
-        space_rations = even_divide(available_width, total_grow if available_width >= 0 else total_shrink)
-        at_x = 0
-        for flex in children:
-            child_min_width, child_min_height = flex.node.min_size(frame.measure_text, box.rect) if flex.basis else Rect(0, 0)
-            child_box = Box(
-                width=child_min_width + sum(space_rations.pop() for _ in range(flex.grow if available_width >= 0 else flex.shrink)),
-                height=row_height,
+        row_rect = Rect(box.width, data.bounding_rect.height)
+        row_box = Box.from_rect(row_rect, box.position + Coordinate(0, at_y))
+        results.append(
+            _hbox_flex_render(
+                tuple(data.flex_children),
+                frame.shrink_to(row_box),
+                row_box
             )
-            child_box = child_box.offset_by(box.position + Coordinate(at_x, at_y))
-            res.add_children_after([flex.node.render(frame.shrink_to(child_box.intersect(box)), child_box)])
-            at_x += child_box.width
-        at_y += row_height
+        )
+        at_y += data.bounding_rect.height
+    res.add_children_after(results)
     return res
