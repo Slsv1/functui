4388a5b76c6667ad28545eaea0936c3bd6ff5a61
Viacheslav Andrushenko <133392473+Slsv1@users.noreply.github.com>
2026-08-24 23:48:45 +0200

    version bump and copyright change

--- docs/conf.py
+++ docs/conf.py
@@ -11,8 +11,8 @@ sys.path.insert(0, os.path.abspath('..'))
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'functui'
-copyright = '2026, Slsv15'
-author = 'Slsv15'
+copyright = '2026, Viacheslav Andrushenko'
+author = 'Viacheslav Andrushenko'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
